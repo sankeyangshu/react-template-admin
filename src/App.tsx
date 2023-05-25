9568c37ecc8cd2e4593b1a8b7639d3f62ff3e4fe
sankeyangshu <1260323835@qq.com>
2023-05-25 14:05:04 +0800

    feat: 获取antd主题变量

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import { ConfigProvider } from 'antd';
+import { ConfigProvider, theme, ThemeConfig } from 'antd';
 import { HashRouter } from 'react-router-dom';
 import 'dayjs/locale/zh-cn';
 import zhCN from 'antd/locale/zh_CN';
 import Router from './routers';
 
+// 获取暗黑主题和默认主题
+const { darkAlgorithm, defaultAlgorithm } = theme;
+
+const isDark = false;
+
 const App = () => {
+  // Ant Design主题变量
+  const antdTheme: ThemeConfig = {
+    // 亮色/暗色配置
+    algorithm: isDark ? darkAlgorithm : defaultAlgorithm,
+  };
+
   return (
-    <ConfigProvider locale={zhCN}>
+    <ConfigProvider locale={zhCN} theme={antdTheme}>
       <HashRouter>
         <Router />
       </HashRouter>
