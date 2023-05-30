52ce48ca83a5040167ad36d6a7724e6aad02adcb
sankeyangshu <1260323835@qq.com>
2023-05-30 17:12:54 +0800

    feat: 新增通知组件

---
+++ src/layouts/components/Header/components/Notice/index.tsx
@@ -0,0 +1,74 @@
+import { Popover, Tooltip, Badge, Tabs } from 'antd';
+import { BellOutlined } from '@ant-design/icons';
+import type { TabsProps } from 'antd';
+import logo from '@/assets/images/logo.png';
+import noMessage from '@/assets/images/noMessage.png';
+import moduleCss from './index.module.less';
+
+/**
+ * 通知组件
+ */
+const noticeItem = (
+  <div className={moduleCss['message-list']}>
+    <div className={moduleCss['message-item']}>
+      <img src={logo} alt="" className={moduleCss['message-img']} />
+      <div className={moduleCss['message-content']}>
+        <span className={moduleCss['message-title']}>这是一个通知！！！</span>
+        <span className={moduleCss['message-date']}>一分钟前</span>
+      </div>
+    </div>
+  </div>
+);
+
+/**
+ * 暂无消息组件
+ */
+const noMessageItem = (
+  <div className={moduleCss['message-empty']}>
+    <img src={noMessage} alt="notData" className={moduleCss['message-noImg']} />
+    <div>暂无消息</div>
+  </div>
+);
+
+// 切换tab
+const onChange = (key: string) => {
+  console.log(key);
+};
+
+// tab选项卡列表
+const tabItems: TabsProps['items'] = [
+  {
+    key: 'first',
+    label: `通知(5)`,
+    children: noticeItem,
+  },
+  {
+    key: 'second',
+    label: `消息(0)`,
+    children: noMessageItem,
+  },
+  {
+    key: 'third',
+    label: `代办(0)`,
+    children: noMessageItem,
+  },
+];
+
+// tab标签
+const tabsContent = <Tabs defaultActiveKey="first" items={tabItems} onChange={onChange} />;
+
+const Notice = () => {
+  return (
+    <>
+      <Popover content={tabsContent} placement="bottom" trigger="click">
+        <Tooltip title="通知" placement="bottom">
+          <Badge count={5} className="icon-style">
+            <BellOutlined />
+          </Badge>
+        </Tooltip>
+      </Popover>
+    </>
+  );
+};
+
+export default Notice;
--- src/layouts/components/Header/index.tsx
+++ src/layouts/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { Layout } from 'antd';
 import User from './components/User';
 import Setting from './components/Setting';
 import ScreenFull from './components/ScreenFull';
+import Notice from './components/Notice';
 import './index.less';
 
 // 获取头部组件
@@ -14,6 +15,8 @@ const LayoutHeader = () => {
       <div className="header-right">
         {/* 全屏 */}
         <ScreenFull />
+        {/* 通知 */}
+        <Notice />
         {/* 设置 */}
         <Setting />
         {/* 用户信息 */}
