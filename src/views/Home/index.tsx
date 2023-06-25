08472989ff9aed74df8abc6d7cfd2d4607c4a775
sankeyangshu <1260323835@qq.com>
2023-06-25 09:45:31 +0800

    feat: 新增欢迎页

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,9 +1,27 @@
-import { Pagination } from 'antd';
+import { Avatar, Card } from 'antd';
+import { getTimeStateStr, welcome } from '@/utils';
+import moduleCss from './index.module.less';
+
 const Home = () => {
+  // 头像
+  const avatarUrl = 'https://img.yzcdn.cn/vant/cat.jpeg';
+
   return (
-    <div>
-      <Pagination defaultCurrent={6} total={500} />
-    </div>
+    <>
+      <div className={moduleCss.home}>
+        <Card hoverable>
+          <div className={moduleCss['page-header']}>
+            <Avatar size={64} src={avatarUrl} />
+            <div className={moduleCss['page-header-tip']}>
+              <p className={moduleCss['page-header-tip-title']}>
+                {`${getTimeStateStr()} xxx，${welcome()}`}
+              </p>
+              <p className={moduleCss['page-header-tip-desc']}>React-Admin后台管理系统</p>
+            </div>
+          </div>
+        </Card>
+      </div>
+    </>
   );
 };
 
