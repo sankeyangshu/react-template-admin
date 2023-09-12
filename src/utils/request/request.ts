9d6c5b9886719184caca2f550cdc11e798d3ed3d
sankeyangshu <1260323835@qq.com>
2023-09-12 08:42:49 +0800

    fix: 修复封装axios的文件中无法获取token的错误

--- src/utils/request/request.ts
+++ src/utils/request/request.ts
@@ -16,7 +16,7 @@ service.interceptors.request.use(
   (config) => {
     // 发请求前做的一些处理，数据转化，配置请求头，设置token,设置loading等
     // 每次发送请求之前判断pinia中是否存在token,如果存在，则统一在http请求的header都加上token，这样后台根据token判断你的登录情况
-    const token = useSettingStore((state) => state.token);
+    const token = useSettingStore.getState().token;
 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
