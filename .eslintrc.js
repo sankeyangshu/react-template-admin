ab7e1d4ab6e0e5645064ab2a1c88c8b3588cfd02
sankeyangshu <1260323835@qq.com>
2023-09-27 08:53:41 +0800

    chore: 新增eslint导入导出规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
       jsx: true,
     },
   },
-  plugins: ['react', '@typescript-eslint', 'react-hooks', 'react-refresh'],
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'react-refresh', 'simple-import-sort'],
 
   /**
    * "off" 或 0    ==>  关闭规则
@@ -35,5 +35,41 @@ module.exports = {
     '@typescript-eslint/no-non-null-assertion': 'off', // 不允许使用后缀运算符的非空断言(!)
     'react-hooks/rules-of-hooks': 'off',
     'react-hooks/exhaustive-deps': 'off',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          [
+            // 以字母(或数字或下划线)或“@”后面跟着字母开头的东西,通常为内置模块引入
+            '^@?\\w',
+            // 内部导入 "@/"
+            '^@(/.*|$)',
+            `^@/assets$`,
+            `^@/components$`,
+            `^@/config$`,
+            `^@/hooks$`,
+            `^@/plugins$`,
+            `^@/routers$`,
+            `^@/store$`,
+            `^@/styles$`,
+            `^@/types$`,
+            `^@/utils$`,
+            // 父级导入. 把 `..` 放在最后.
+            '^\\.\\.(?!/?$)',
+            '^\\.\\./?$',
+            // 同级导入. 把同一个文件夹.放在最后
+            '^\\./(?=.*/)(?!/?$)',
+            '^\\.(?!/?$)',
+            '^\\./?$',
+            // 样式导入.
+            '^.+\\.?(css|less|scss)$',
+            // 带有副作用导入，比如import 'a.css'这种.
+            '^\\u0000',
+          ],
+        ],
+      },
+    ],
+    'simple-import-sort/exports': 'error', // 导出
+    'import/order': 'off',
   },
 };
